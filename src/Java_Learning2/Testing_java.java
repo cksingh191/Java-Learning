25674af9905d93cdf7e2aa0a158242f0d0878dc4
Chandrashekhar Singh <c.shekhar191@gmail.com>
2019-12-15 13:13:07 +0530

    hhhh

---
+++ src/Java_Learning2/Testing_java.java
@@ -0,0 +1,16 @@
+package Java_Learning2;
+
+public class Testing_java {
+
+	public static void main(String[] args) {
+		System.out.println("Testing java");
+		  
+
+	}
+    public void login() {
+	 System.out.println("Login");
+ }
+    public void login1() {
+   	 System.out.println("Login");
+    }
+}
